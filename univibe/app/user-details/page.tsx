50717b0153da4f234120d92b44831ab6cbabdbe2
Mahir <skmahirashef04@gmail.com>
2026-04-04 21:51:00 +0530

    Fix:Route fix

--- univibe/app/user-details/page.tsx
+++ univibe/app/user-details/page.tsx
@@ -2,19 +2,23 @@ import UserDetailsForm from "@/components/layouts/UserDetailsForm"
 import { headers } from "next/headers"
 import { auth } from "@/lib/auth"
 import { redirect } from "next/navigation"
+import { prisma } from "@/lib/prisma"
 
-const UserDetails = async() => {
-    const session = await auth.api.getSession({
-        headers: await headers(),
-    });
+const UserDetails = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
 
-    if (!session) {
-        redirect("/auth/login")
-    }
+  if (!session) redirect("/auth/login")
+ 
+  const profile = await prisma.profile.findUnique({
+    where: { userId: session.user.id },
+    select: { id: true },
+  })
 
-    return (
-        <UserDetailsForm />
-    )
+  if (profile) redirect("/")
+
+  return <UserDetailsForm />
 }
 
-export default UserDetails;
+export default UserDetails
