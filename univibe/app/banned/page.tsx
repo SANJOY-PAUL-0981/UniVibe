957bc965ef10e3cd3ac56a7d98eef98213d6e777
SANJOY <paulsanjoy2923@gmail.com>
2026-04-21 13:19:02 +0530

    banned page added

--- univibe/app/banned/page.tsx
+++ univibe/app/banned/page.tsx
@@ -1,26 +1,33 @@
-import Link from "next/link"
 import Image from "next/image"
+import Link from "next/link"
 
-export default function NotFound() {
+const BannedPage = () => {
     return (
         <div className="h-screen w-screen flex justify-center items-center text-center">
             <div className="flex flex-col items-center gap-5">
                 <Image
-                    src="/crying-banana-404.gif"
+                    src="/scuba-cat-banned.gif"
                     alt="404 scuba cat"
                     width={200}
                     height={200}
                 />
                 <p className="font-bold text-5xl">
-                    404
+                    403
                 </p>
 
                 <div className="text-xs font-semibold text-muted-foreground">
                     <p>
-                        Our site only has 4 pages.
+                        Univibe said: nah, not today
                     </p>
                     <p>
-                        How the f*ck did you end up here?
+                        You speedran getting banned. New record? 🏆
+                    </p>
+                    <p>
+                        We’ve seen bots behave better than you.
+                    </p>
+                    <p>
+                        Take 48 hours off.
+                        The app needs a break from you.
                     </p>
                 </div>
 
@@ -33,3 +40,5 @@ export default function NotFound() {
         </div>
     )
 }
+
+export default BannedPage
